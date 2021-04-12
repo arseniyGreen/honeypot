b89fb5121ec54baa2c166cbba28ca5b25b2e83c0
Arseniy Arkadskov <ars.ark111@yandex.ru>
2021-04-12 16:34:03 +0300

    updated

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Honeypot page"
+    return "You've fucked up. I've got your ip: " request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 
 if __name__ == "__main__":
     app.run(debug=True)
