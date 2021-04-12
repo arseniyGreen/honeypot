fcf217bd3f0d6a12d8a056eaa1c97edf1803ec54
Arseniy Arkadskov <ars.ark111@yandex.ru>
2021-04-12 21:36:18 +0300

    updated

--- netwatch.py
+++ netwatch.py
@@ -2,6 +2,8 @@ import os
 
 print("Scanning...")
 
+choice = "n"
+
 while(1):
     status = os.system("netstat | grep 4444")
 
@@ -10,4 +12,16 @@ while(1):
         os.system("sleep 7")
     else:
         print("We've been hacked!")
-        os.system("Sleep 4")
+        print("Going to kill all www-data processes. Continue? [y/n]")
+        choice = input()
+        if(choice == "y"):
+            os.system("sudo killall --user www-data")
+            if(os.system("sudo killall --user www-data") == 0):
+                print("Success!")
+            else:
+                print("Error occured!")
+        elif(choice == "n"):
+            print("Exiting...")
+            break
+        else:
+            print("Invalid input!")
