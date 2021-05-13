b871f92a17fcef79c482266901d43f35f6d574b3
Arseniy Arkadskov <ars.ark111@yandex.ru>
2021-05-13 23:17:01 +0300

    Update netwatch.py

--- netwatch.py
+++ netwatch.py
@@ -13,20 +13,20 @@ choice = "N"
 if "root" in rootCheck:
     print("Scanning...")
 
-while(True):
-    status = str(subprocess.run(["yara", "rule.yara", "/opt/zeek/logs/current"], capture_output = True))
-    if "meterpreter" in status:
-        print("Compromise detected!")
-        choice = input("Going to kill all www-data processes. Proceed?[Y/N]")
-        if(choice == "Y"):
-            os.system("sudo killall --user www-data")
-            print("Done!")
-            break
+    while(True):
+        status = str(subprocess.run(["yara", "rule.yara", "/opt/zeek/logs/current"], capture_output = True))
+        if "meterpreter" in status:
+            print("Compromise detected!")
+            choice = input("Going to kill all www-data processes. Proceed?[Y/N]")
+            if(choice == "Y"):
+                os.system("sudo killall --user www-data")
+                print("Done!")
+                break
+            else:
+                print("Exiting...")
+                break
         else:
-            print("Exiting...")
-            break
-    else:
-        print("No suspicious activity found")
-        os.system("sleep 5")
+            print("No suspicious activity found")
+            os.system("sleep 5")
 else:
     print("Not a root user! Please use sudo to run this script.")
